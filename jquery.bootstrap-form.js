d92a2b3bc31abd6617b3449e90eaa77a6adb5f3e
Vlad Miller <vmiller@meta-labs.org>
2012-07-06 15:31:39 -0400

    Bug fixes + improved code

--- jquery.bootstrap-form.js
+++ jquery.bootstrap-form.js
@@ -8,14 +8,21 @@
 ;
 (function($) {
     var validators = {
-        'string':  /^[\w\d\s]*$/i,
-        'integer': /^[\d]*$/i,
-        'email':   /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i,
+        'string'         : /^[^<>'"]*$/i,
+        'integer'        : /^[\d]*$/i,
+        'email'          : /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i,
+        'true'           : /^true$/i,
+        'bankCardNumber' : /\d/i
     }
 
     function notify($object, validator, type) {
         var $error = $object.attr(validator + ':' + type) !== undefined ? $object.attr(validator + ':' + type) : $object.attr(type);
-        $object.next('.input-status').html($error);
+        var $holder = $object.next('.input-status');
+        if ($object.attr('holder') !== undefined) {
+            $holder = $('#' + $object.attr('holder'));
+        }
+
+        $holder.html($error);
         $object.parents('.control-group').addClass(type);
 
         switch (type) {
@@ -28,9 +35,7 @@
             case 'sucess': {
                 return 2;
             }
-
         }
-
     }
 
     $.fn.validate = function() {
@@ -39,8 +44,18 @@
 
         $('input,select,textarea', self).each(function() {
             var $input   = $(this);
-            var $pattern = $input.attr('validate');
-            var $value   = $input.val();
+            var $pattern     = $input.attr('validate');
+            var $notValidate = $input.attr('notValidate');
+            var $value       = $input.val();
+            var type         = $input.attr('type');
+
+            if ($notValidate == 1) {
+                return true;
+            }
+
+            if (type == 'checkbox') {
+                $value = $input.is(':checked');
+            }
 
             $input.next('.input-status').html('');
             $input.parents('.control-group')
@@ -56,21 +71,9 @@
                     var $type      = $piece[1];
 
                     switch ($validator) {
-                        case 'string': {
-                            if (!validators[$validator].test($value)) {
-                                var level = notify($input, $validator, $type);
-                                if (level == 2 && valid > 1) {
-                                    valid = 2;
-                                }
-                                if (level == 1 && valid > 0) {
-                                    valid = 1;
-                                }
-                                if (level == 0) {
-                                    valid = 0;
-                                }
-                            }
-                            break;
-                        }
+                        case 'string':
+                        case 'email':
+                        case 'bankCardNumber':
                         case 'integer': {
                             if (!validators[$validator].test($value)) {
                                 var level = notify($input, $validator, $type);
@@ -88,21 +91,6 @@
                         }
                         case 'notEmpty': {
                             if ($value === undefined || $value == '') {
-                                var level = notify($input, $validator, $type);
-                                if (level == 2 && valid > 1) {
-                                    valid = 2;
-                                }
-                                if (level == 1 && valid > 0) {
-                                    valid = 1;
-                                }
-                                if (level == 0) {
-                                    valid = 0;
-                                }
-                            }
-                            break;
-                        }
-                        case 'email': {
-                            if (!validators[$validator].test($value)) {
                                 var level = notify($input, $validator, $type);
                                 if (level == 2 && valid > 1) {
                                     valid = 2;
